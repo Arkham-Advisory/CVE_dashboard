9fe726cbdc17698d7fd0c8269dc4ec6353e67373
Niels Freier <niels.freier@ba.com>
2026-03-17 10:09:49 +0000

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CVE_dashboard/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
